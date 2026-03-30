bb5d4ac52b3ead3d27219a0f53068841b08a4953
kaushaltomar-hub <kaushaltomar2712@gmail.com>
2026-03-30 13:51:32 +0530

    updated CORS config

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://wt-project-five.vercel.app"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Routes
